c108864d2a3f2b2b123691bf73053156a58a7221
Rishi Varman Rathimala Thangaravi <rs329673@dal.ca>
2024-08-07 10:47:27 -0300

    cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const xss = require("xss-clean");
 const bodyParser = require('body-parser');
-const s3 = new AWS.S3();
 const { fetchSecrets, configureAWS } = require('./config');
 const { createTables } = require("./db/dynamo");
 
@@ -31,24 +30,19 @@ app.use("/", limiter);
 
 app.use(express.json());
 app.use(xss());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const emitter = new EventEmitter();
 emitter.setMaxListeners(20);
+
+// Configure CORS
 app.use(cors());
 
 app.use(userRoutes);
 app.use(subjectRoutes);
 app.use(attendanceRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
-// Initialize and start the app
 const initializeApp = async () => {
   try {
     const secrets = await fetchSecrets();
@@ -62,22 +56,11 @@ const initializeApp = async () => {
     subjectModel.init({ docClient });
     attendanceModel.init({ docClient });
 
-    // Your application setup (middlewares, routes, etc.)
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(bodyParser.json({ limit: '50mb' }));
-    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
     // Create DynamoDB tables on startup
     await createTables();
 
-    // Define routes
-    app.use(userRoutes);
-    app.use(subjectRoutes);
-    app.use(attendanceRoutes);
-
     // Start the server
-    const PORT =  5000;
+    const PORT = 5000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
